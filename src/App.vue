623c2045c2ff85378ccf8524d3d6ef0d2a45a5e7
hahei89 <18702971504@163.com>
2021-08-12 22:18:19 +0800

    button的类型属性

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
-   simple vue ui
+    <div class="row">
+      <simple-button type="primary">登录</simple-button>
+      <simple-button type="success">注销</simple-button>
+      <simple-button type="info">删除</simple-button>
+      <simple-button type="warning">按钮</simple-button>
+      <simple-button type="danger">按钮</simple-button>
+    </div>
   </div>
 </template>
 
@@ -11,6 +17,11 @@ export default {
 }
 </script>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+  .row {
+    margin-bottom: 20px;
+    .hm-button {
+      margin-right: 20px;
+    }
+  }
 </style>
